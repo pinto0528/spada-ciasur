0a37398c4aed015537adc18682d068571d0cfe32
Pinto <nclpnt0528@gmail.com>
2024-07-29 17:42:50 -0300

    Config frontend

---
+++ frontend/spada/src/app/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* Gráficas y estadísticas */}
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ frontend/spada/src/app/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* Formulario de login */}
+    </div>
+  );
+};
+
+export default LoginPage;
