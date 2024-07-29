7c3be0bae1902ca9be9585f5fdf60e828b371ed4
Pinto <nclpnt0528@gmail.com>
2024-07-29 18:11:47 -0300

    Fix front working

---
+++ frontend/spada/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Login Page
+        </p>
+      </div>
+    </main>
+  );
+}
